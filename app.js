f95b8a24fbef1e84ec649dd39952ad70fe8df45c
batesjernigan <bates1012@aol.com>
2015-04-14 18:33:42 -0400

    Added some some fields the the review section, played with the formatting to get a better feel for how it would look. Created a gallerycontroller to control where the current index of the gallery is. May rename this as I think it's misleading to say.

--- app.js
+++ app.js
@@ -16,7 +16,19 @@
       "assets/images/azurite/azurite-01-thumb.gif",
       "assets/images/azurite/azurite-02-thumb.gif",
       "assets/images/azurite/azurite-03-thumb.gif"
-    ]
+    ],
+    reviews: [{
+      stars: 5,
+      body: "I love this gem!",
+      author: "joe@example.org",
+      createdOn: 1397490980837
+    }, {
+      stars: 1,
+      body: "This gem sucks.",
+      author: "tim@example.org",
+      createdOn: 1397490980837
+    }]
+
   }, {
     name: 'Bloodstone',
     description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
@@ -29,7 +41,18 @@
       "assets/images/bloodstone/bloodstone-01-thumb.gif",
       "assets/images/bloodstone/bloodstone-02-thumb.gif",
       "assets/images/bloodstone/bloodstone-03-thumb.gif"
-    ]
+    ],
+    reviews: [{
+      stars: 3,
+      body: "I think this gem was just OK, could honestly use more shine, IMO.",
+      author: "JimmyDean@example.org",
+      createdOn: 1397490980837
+    }, {
+      stars: 4,
+      body: "Any gem with 12 faces is for me!",
+      author: "gemsRock@example.org",
+      createdOn: 1397490980837
+    }]
   }, {
     name: 'Zircon',
     description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
@@ -42,7 +65,23 @@
       "assets/images/zircon/zircon-01-thumb.gif",
       "assets/images/zircon/zircon-02-thumb.gif",
       "assets/images/zircon/zircon-03-thumb.gif"
-    ]
+    ],
+    reviews: [{
+      stars: 1,
+      body: "This gem is WAY too expensive for its rarity value.",
+      author: "turtleguyy@example.org",
+      createdOn: 1397490980837
+    }, {
+      stars: 1,
+      body: "BBW: High Shine != High Quality.",
+      author: "LouisW407@example.org",
+      createdOn: 1397490980837
+    }, {
+      stars: 1,
+      body: "Don't waste your rubles!",
+      author: "nat@example.org",
+      createdOn: 1397490980837
+    }]
   }];
 
   app.controller("PanelController", function(){
@@ -52,6 +91,13 @@
     };
     this.isSelected = function(checkTab){
       return this.tab === checkTab;
+    };
+  });
+
+  app.controller("GalleryController", function(){
+    this.current = 0;
+    this.setCurrent = function(new_current){
+      this.current = new_current || 0;
     };
   });
 
